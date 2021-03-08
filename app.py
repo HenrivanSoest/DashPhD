ca719db1857c90cc3c8ac81d2477e7c890381819
HenrivanSoest <77961837+HenrivanSoest@users.noreply.github.com>
2021-03-08 17:59:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
+
+got_df = pd.read_csv(r'C:\Users\Henri van Soest\Documents\PhD\Writing\Year 3 Writeup\4 Framework\Framework_Timeline2.csv')
+
+print(got_df)
 
 top_markdown_text = '''
 This is my first deployed app
